011ebdbbafef91544c2efec400e9637bc77e711a
asmit <asmithrajbhandari@gmail.com>
2018-10-25 10:02:29 +0545

    migration for category

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   	resources :articles do
 	    resources :comments
     end
+    resources :categories
   	get 'blog/new', to: 'blog#new'
 	post 'blog/new', to: 'blog#create'
   	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

25bab1b0b9bf01d911d7fb16e4d6478b055d4de3
asmith41 <asmithrajbhandari@gmail.com>
2018-10-25 21:50:14 +0545

    s

--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,8 @@ ActiveRecord::Schema.define(version: 20181024120812) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "bls", force: :cascade do |t|

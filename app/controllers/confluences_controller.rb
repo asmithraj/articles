3f1e0019b206946708d6bd244fb22a7b68321fb6
asmithraj <asmithrajbhandari@gmail.com>
2018-10-30 18:04:34 +0545

    confluence

--- app/controllers/confluences_controller.rb
+++ app/controllers/confluences_controller.rb
@@ -1,11 +1,20 @@
 class ConfluencesController < ApplicationController
 
-	def getcontent
+	def getspacelist
 		@response = RestClient::Request.execute(
 			method: :get,
-			url: 'https://cloudchomp.atlassian.net/wiki/rest/api/content/',
-			headers: {Authorization: 'Basic YXNtaXQucmFqYmhhbmRhcmlAY2xvdWRjaG9tcC5jb206cHJvQ0FCOTkrKw=='}
+			url: 'https://cloudchomp.atlassian.net/wiki/rest/api/space/',
+			headers: { Authorization: 'Basic YXNtaXQucmFqYmhhbmRhcmlAY2xvdWRjaG9tcC5jb206cHJvQ0FCOTkrKw==' }
 		)
-		# @response = RestClient.get( 'https://cloudchomp.atlassian.net/wiki/rest/api/content/', headers: {Authorization: 'Basic YXNtaXQucmFqYmhhbmRhcmlAY2xvdWRjaG9tcC5jb206cHJvQ0FCOTkrKw=='})
+
 	end
+
+	def getspacechildren
+		@response = RestClient::Request.execute(
+			method: :get,
+			url: 'https://cloudchomp.atlassian.net/wiki/rest/api/content/5177345/child/page?expand=children.page',
+			headers: { Authorization: 'Basic YXNtaXQucmFqYmhhbmRhcmlAY2xvdWRjaG9tcC5jb206cHJvQ0FCOTkrKw==' }
+		)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,24 @@
 Rails.application.routes.draw do
-  # get 'users/new'
-  # get 'users/create'
-  # get 'sessions/new'
-  # get 'sessions/create'
-  # get 'sessions/destroy'
-  # get 'dashboard/show'
-  # get 'categories/new'
+  
+  root 'articles#index'
   get 'welcome/index'
   resources :sessions, only: [:create]
   resources :users, only: [:new, :create]
-  root 'articles#index'
   get 'login', to: 'sessions#new'
   get 'logout', to: 'sessions#destroy'
   get 'users', to: 'users#index'
   patch '/users/:id', to: 'users#update', as: 'user_update'
   get '/confluence', to: 'confluences#index'
-  get '/confluence/content', to: 'confluences#getcontent'
-	# root 'welcome#index'
+  get '/confluence/space', to: 'confluences#getspacelist'
+  get 'confluence/children', to: 'confluences#getspacechildren'
 	
 	resources :articles do
     resources :comments
   end
+
   resources :categories do
-  	#resources :articles
-  	# get 'categoryshow', :on => :collection
   end
+
 	get 'categories/:category_id/articles', to: 'articles#showcategoryarticles', as: 'show_all'
   get 'blog/new', to: 'blog#new'
 	post 'blog/new', to: 'blog#create'
